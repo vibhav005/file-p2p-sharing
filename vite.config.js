88f84f2d588bfa9ffd221c21356d244e4313cc8c
vibh005 <veebhavjoshi1@gmail.com>
2025-03-10 12:34:02 +0530

    Add build file

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/file-p2p-sharing/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
