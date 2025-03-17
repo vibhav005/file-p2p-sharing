734e4e432ea3a19537d39fa45b5e2fa056e1cf9f
vibh005 <veebhavjoshi1@gmail.com>
2025-03-17 14:37:03 +0530

    add more ice servers

--- src/App.jsx
+++ src/App.jsx
@@ -67,6 +67,15 @@ function App() {
   useEffect(() => {
     const peer = new Peer({
       debug: 2,
+      config: {
+        iceServers: [
+          { urls: "stun:stun.l.google.com:19302" },
+          { urls: "stun:stun1.l.google.com:19302" },
+          { urls: "stun:stun2.l.google.com:19302" },
+          { urls: "stun:stun3.l.google.com:19302" },
+          { urls: "stun:stun4.l.google.com:19302" },
+        ],
+      },
     });
 
     peer.on("open", (id) => {
